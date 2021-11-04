a35bbabe8eb169f58a80a5901b7cd481d1fe1708
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-11-04 14:21:22 -0300

    cria o animal gato e sua classe test

---
+++ src/models/Gato.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Gato extends Animal{
+    public Gato(){}
+    public Gato(String nome){
+        super(nome);
+    }
+    public String mia(){
+        return "miau";
+    }
+}
---
+++ src/test/TesteGato.java
@@ -0,0 +1,10 @@
+package test;
+
+import models.Gato;
+
+public class TesteGato {
+    public static void main(String[] args) {
+        Gato gato = new Gato("gatinho");
+        System.out.println(gato.mia());
+    }
+}
