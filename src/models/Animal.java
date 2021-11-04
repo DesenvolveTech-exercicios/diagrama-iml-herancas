505fff0744af2379994a99d3a57501d315abe261
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-11-04 14:06:13 -0300

    cria o objeto animal e implementa teste

---
+++ src/models/Animal.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Animal {
+    private String nome;
+    private String raca;
+
+    public Animal() {}
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String caminha(){
+        return "Caminhando";
+    }
+}
---
+++ src/test/TesteAnimal.java
@@ -0,0 +1,10 @@
+package test;
+
+import models.Animal;
+
+public class TesteAnimal {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(animal.caminha());
+    }
+}
